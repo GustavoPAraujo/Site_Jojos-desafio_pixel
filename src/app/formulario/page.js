55d3bfd97ffb2f709c0600b1ebccb5c6323ebf05
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-07-18 17:31:22 -0300

    styles(formulario page): fixing styles

--- src/app/formulario/page.js
+++ src/app/formulario/page.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
-import styles from './Formulario.module.css'; // Importar o CSS
+import styles from './Formulario.module.css';
 
 const Formulario = () => {
   const [submitted, setSubmitted] = useState(false);
@@ -17,43 +17,37 @@ const Formulario = () => {
   };
 
   return (
-    <>
+    <div className={styles.pageContainer}>
       <Header />
 
       <main className={styles.main}>
-        {submitted ? (
-          <div className={styles.successMessage}>
-            <h1>Formulário enviado com sucesso</h1>
-          </div>
-        ) : (
-          <>
-            <h1 className={styles.formTitle}>Formulário de Vaga</h1>
 
-            <form onSubmit={handleSubmit} className={styles.form}>
-              <label className={styles.label}>
-                Nome:
-                <input type="text" name="name" required className={styles.input} />
-              </label>
-              <label className={styles.label}>
-                Email:
-                <input type="email" name="email" required className={styles.input} />
-              </label>
-              <label className={styles.label}>
-                Link para portfólio:
-                <input type="url" name="portfolio" className={styles.input} />
-              </label>
-              <label className={styles.label}>
-                Por que você quer fazer parte da Jojos?
-                <textarea name="reason" required className={styles.textarea}></textarea>
-              </label>
-              <button type="submit" className={styles.button}>Enviar formulário</button>
-            </form>
-          </>
-        )}
+        <h1 className={styles.formTitle}>Formulário de Vaga</h1>
+
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <label className={styles.label}>
+            Nome:
+            <input type="text" name="name" required className={styles.input} />
+          </label>
+          <label className={styles.label}>
+            Email:
+            <input type="email" name="email" required className={styles.input} />
+          </label>
+          <label className={styles.label}>
+            Link para portfólio:
+            <input type="url" name="portfolio" className={styles.input} />
+          </label>
+          <label className={styles.label}>
+            Por que você quer fazer parte da Jojos?
+            <textarea name="reason" required className={styles.textarea}></textarea>
+          </label>
+          <button type="submit" className={styles.button}>Enviar formulário</button>
+        </form>
+
       </main>
 
       <Footer />
-    </>
+    </div>
   );
 };
 
