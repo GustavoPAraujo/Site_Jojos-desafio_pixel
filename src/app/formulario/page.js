ccf0bc6b913e950c272283784a5d853f918f9500
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-07-17 22:28:42 -0300

    feat(formulario page): create base structure

---
+++ src/app/formulario/page.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+const Formulario = () => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Lógica para enviar o formulário
+    setSubmitted(true);
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>JOJOS</h1>
+        <nav>
+          <Link href="/jogos">Jogos</Link>
+          <Link href="/sobre">Sobre</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/formulario">Carreira</Link>
+        </nav>
+      </header>
+
+      <main>
+        {submitted ? (
+          <div>
+            <h2>Formulário enviado com sucesso</h2>
+          </div>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <h2>Formulário de Vaga</h2>
+            <label>
+              Nome:
+              <input type="text" name="name" required />
+            </label>
+            <label>
+              Email:
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              Link para portfólio:
+              <input type="url" name="portfolio" />
+            </label>
+            <label>
+              Por que você quer fazer parte da Jojos?
+              <textarea name="reason" required></textarea>
+            </label>
+            <button type="submit">Enviar formulário</button>
+          </form>
+        )}
+      </main>
+
+      <footer>
+        <p>&copy; 2024 JOJOS GAME STUDIOS. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Formulario;
