a6b5971aa0bbe60dff35821d6755730adf28af50
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-07-20 16:52:07 -0300

    style: form submit styles

--- src/components/FormSubmit/FormSubmit.js
+++ src/components/FormSubmit/FormSubmit.js
@@ -1,11 +1,13 @@
 
 import styles from './FormSubmit.module.css';
+import Image from 'next/image';
+
 
 const FormSubmit = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Formulário enviado com sucesso!</h1>
-            <img src='/Vertor.png' alt='sucsses'/>
+            <Image src='/Vector.png' alt='sucsses' height={120} width={100}/>
         </div>
     );
 };
