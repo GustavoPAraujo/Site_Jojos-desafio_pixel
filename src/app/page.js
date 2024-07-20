fc79bcecaf22535a1423c1e7409d455fbd38a849
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-07-20 00:39:25 -0300

    feat(about): content

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,13 @@
-
 import Link from 'next/link';
-import Image from 'next/image';
 
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 
-import styles from './page.module.css';
-import { ImageConfigContext } from 'next/dist/shared/lib/image-config-context.shared-runtime';
+import styles from './Page.module.css';
 
 export default function Home() {
-
   return (
-    <div>
+    <div className={styles.pageContainer}>
       <Header />
 
       <main className={styles.main}>
@@ -26,17 +22,22 @@ export default function Home() {
         </div>
 
         <div className={styles.sobre}>
+
           <h1>Sobre Nós</h1>
-          <div>
+
+          <div className={styles.sobreContent}>
             <div>
               <p>
                 Bem-vindo à Jojos, uma empresa de jogos retrô brasileira! Somos uma equipe apaixonada por games clássicos e estamos comprometidos em trazer de volta a magia desses títulos que marcaram época. Com gráficos pixelizados, trilhas sonoras envolventes e mecânicas desafiadoras, nossos jogos são verdadeiras homenagens aos consoles e computadores que encantaram o passado. Junte-se a nós e embarque em uma viagem nostálgica repleta de aventuras.
+              </p>
+              <p>
                 Acreditamos que os jogos retrô têm o poder de unir gerações, despertar memórias afetivas e proporcionar momentos de pura diversão. Nossa missão é manter viva a essência dos jogos clássicos, levando você a uma jornada inesquecível pelos mundos pixelizados cheios de magia. Faça parte dessa nova era retrô e mergulhe em experiências que continuam a encantar corações até hoje.
+              </p>
+              <p>
                 Jojos, onde a nostalgia encontra a diversão! Com uma equipe apaixonada por games, estamos comprometidos em trazer de volta a magia dos jogos clássicos que marcaram gerações inteiras. Explore nossos jogos e embarque em uma viagem nostálgica repleta de aventuras e desafios. Junte-se a nós e compartilhe da nossa paixão pelos jogos retrô!
               </p>
             </div>
-            <Image src='/sobreFoto.png' width={500} height={500} alt='Sobre' />
-
+            <img src='/sobreFoto.png' alt='Sobre' />
           </div>
         </div>
 
@@ -55,5 +56,4 @@ export default function Home() {
       <Footer />
     </div>
   );
-};
-
+}
