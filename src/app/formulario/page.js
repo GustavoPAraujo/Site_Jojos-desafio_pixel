12e77a7ffdfcec297fbaf37fc641bea2febd9c66
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-07-20 16:33:13 -0300

    feat: forms submit validation

--- src/app/formulario/page.js
+++ src/app/formulario/page.js
@@ -1,19 +1,39 @@
 "use client";
 
 import { useState } from 'react';
-import Link from 'next/link';
 
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import styles from './Formulario.module.css';
 
 const Formulario = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    portfolio: '',
+    reason: ''
+  });
   const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Lógica para enviar o formulário
     setSubmitted(true);
+    // Limpar o formulário após o envio
+    setFormData({
+      name: '',
+      email: '',
+      portfolio: '',
+      reason: ''
+    });
   };
 
   return (
@@ -21,29 +41,57 @@ const Formulario = () => {
       <Header />
 
       <main className={styles.main}>
-
         <h1 className={styles.formTitle}>Formulário de Vaga</h1>
 
         <form onSubmit={handleSubmit} className={styles.form}>
           <label className={styles.label}>
             Nome:
-            <input type="text" name="name" required className={styles.input} />
+            <input 
+              type="text" 
+              name="name" 
+              required 
+              className={styles.input} 
+              value={formData.name} 
+              onChange={handleChange}
+            />
           </label>
           <label className={styles.label}>
             Email:
-            <input type="email" name="email" required className={styles.input} />
+            <input 
+              type="email" 
+              name="email" 
+              required 
+              className={styles.input} 
+              value={formData.email} 
+              onChange={handleChange}
+            />
           </label>
           <label className={styles.label}>
             Link para portfólio:
-            <input type="url" name="portfolio" className={styles.input} />
+            <input 
+              type="url" 
+              name="portfolio" 
+              className={styles.input} 
+              value={formData.portfolio} 
+              onChange={handleChange}
+            />
           </label>
           <label className={styles.label}>
             Por que você quer fazer parte da Jojos?
-            <textarea name="reason" required className={styles.textarea}></textarea>
+            <textarea 
+              name="reason" 
+              required 
+              className={styles.textarea} 
+              value={formData.reason} 
+              onChange={handleChange}
+            ></textarea>
           </label>
           <button type="submit" className={styles.button}>Enviar formulário</button>
         </form>
 
+        {submitted && (
+          <p className={styles.successMessage}>Obrigado por se candidatar!</p>
+        )}
       </main>
 
       <Footer />
