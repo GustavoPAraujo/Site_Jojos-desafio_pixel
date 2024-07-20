72435bbb5f6657268b77cc6b19e1411f78010e5f
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-07-20 01:05:18 -0300

    feat(new sanity  type): adding type vagaas

--- src/sanity/schema.js
+++ src/sanity/schema.js
@@ -2,7 +2,8 @@ import {blockContentType} from './schemaTypes/blockContentType'
 import {categoryType} from './schemaTypes/categoryType'
 import {postType} from './schemaTypes/postType'
 import {authorType} from './schemaTypes/authorType'
+import {vagasType} from './schemaTypes/vagasType'
 
 export const schema = {
-  types: [blockContentType, categoryType, postType, authorType],
+  types: [blockContentType, categoryType, postType, authorType, vagasType],
 }
---
+++ src/sanity/schemaTypes/vagasType.js
@@ -0,0 +1,23 @@
+export const vagasType = {
+    name: 'vaga',
+    title: 'Vaga',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Nome da Vaga',
+        type: 'string',
+      },
+      {
+        name: 'department',
+        title: 'Departamento',
+        type: 'string',
+      },
+      {
+        name: 'location',
+        title: 'Localização',
+        type: 'string',
+      },
+    ],
+  };
+  
