9c785b30d079acacaa4eb68002e66e2b1f352ae8
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-07-21 23:16:55 -0300

    feat: new sanity type

---
+++ src/sanity/schemaTypes/gameType.js
@@ -0,0 +1,40 @@
+export default {
+    name: 'game',
+    title: 'Game',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+      },
+      {
+        name: 'price',
+        title: 'Price',
+        type: 'number',
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'name',
+          maxLength: 96,
+        },
+      },
+    ],
+  };
+  
